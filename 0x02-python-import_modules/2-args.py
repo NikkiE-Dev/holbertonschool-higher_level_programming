66b44ad71daa7b13faa6ee29dbfdb3d0095a5a57
NikkiE-Holberton <2495@holbertonschool.com>
2021-04-30 21:07:28 +0000

    Adding args file

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+
+n = len(sys.argv)
+a = n - 1
+num = 1
+
+if n == 1:
+    print(a, "arguments.")
+
+else:
+    print(a, "arguments:")
+    while (a >= num):
+        print ("{}: {}".format(num, sys.argv[num]))
+        num = num + 1
