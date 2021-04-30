e0847e95fd27f08af7fb5e7965eea91fba304bf1
NikkiE-Holberton <2495@holbertonschool.com>
2021-04-30 18:43:29 +0000

    Adding line of code

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
 
-a = 10
-b = 5
-total1 = add(a, b)
-total2 = sub(a, b)
-total3 = mul(a, b)
-total4 = div(a, b)
-print("{} + {} = {}".format(a, b, total1))
-print("{} - {} = {}".format(a, b, total2))
-print("{} * {} = {}".format(a, b, total3))
-print("{} / {} = {}".format(a, b, total4))
+    a = 10
+    b = 5
+    total1 = add(a, b)
+    total2 = sub(a, b)
+    total3 = mul(a, b)
+    total4 = div(a, b)
+    print("{} + {} = {}".format(a, b, total1))
+    print("{} - {} = {}".format(a, b, total2))
+    print("{} * {} = {}".format(a, b, total3))
+    print("{} / {} = {}".format(a, b, total4))
