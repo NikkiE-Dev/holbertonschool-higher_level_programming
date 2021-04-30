df3170556ea60c1791775688b2be366f0d327a9f
NikkiE-Holberton <2495@holbertonschool.com>
2021-04-30 21:09:31 +0000

    Adding line of code in args file

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys
 
-n = len(sys.argv)
-a = n - 1
-num = 1
+    n = len(sys.argv)
+    a = n - 1
+    num = 1
 
-if n == 1:
-    print(a, "arguments.")
+    if n == 1:
+        print(a, "arguments.")
 
-else:
-    print(a, "arguments:")
-    while (a >= num):
-        print ("{}: {}".format(num, sys.argv[num]))
-        num = num + 1
+    else:
+        print(a, "arguments:")
+        while (a >= num):
+            print ("{}: {}".format(num, sys.argv[num]))
+            num = num + 1
