6c84bf97a8aee9fa926c815d18f377774034124a
NikkiE-Holberton <2495@holbertonschool.com>
2021-04-30 00:47:13 +0000

    Adding Calculation file

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+
+a = 10
+b = 5
+total1 = add(a, b)
+total2 = sub(a, b)
+total3 = mul(a, b)
+total4 = div(a, b)
+print("{} + {} = {}".format(a, b, total1))
+print("{} - {} = {}".format(a, b, total2))
+print("{} * {} = {}".format(a, b, total3))
+print("{} / {} = {}".format(a, b, total4))
