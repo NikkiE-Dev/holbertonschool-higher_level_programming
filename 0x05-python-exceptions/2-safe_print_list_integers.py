0f7964bb1e30751781aff790df967642fc84ac22
NikkiE-Holberton <2495@holbertonschool.com>
2021-05-03 23:42:06 +0000

    Adding Print list w/error file

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    size = 0
+    try:
+        for i in range(x):
+            if size <= x:
+                if (isinstance(my_list[i], int)) is False:
+                    continue
+                else:
+                    print("{}".format(my_list[i]), end="")
+                    size = size + 1
+        print()
+        return(size)
+    except:
+        return(None)
