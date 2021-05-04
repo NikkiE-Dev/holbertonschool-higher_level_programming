77c908fc9a66877dec106097a37fe6bd894fc696
NikkiE-Holberton <2495@holbertonschool.com>
2021-05-04 04:27:27 +0000

    Adding private attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+class Square:
+    """ This is how you create a simple class."""
+    def __init__(self, size):
+        """this is how you create a private attribute in a class"""
+        self.__size = size
