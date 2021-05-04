83ba70ca743a5e22121977dc8454dc53ecbc552a
NikkiE-Holberton <2495@holbertonschool.com>
2021-05-04 19:44:18 +0000

    Adding PEP8 to module docstring

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """ This module is how you create a class."""
+
+
 class Square:
     """ This is how you create a simple class."""
     def __init__(self, size):
-        """this is how you create a private attribute in a class"""
+        """This is how you create a private attribute in a class"""
         self.__size = size
