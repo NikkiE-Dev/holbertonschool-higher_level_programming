ad634f907981de0d4b8c250cd439e8c1ed98268d
NikkiE-Holberton <2495@holbertonschool.com>
2021-05-04 21:58:51 +0000

    Adding area squared file

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" This is how you create a module."""
+
+
+class Square:
+    """ This is how you create a simple class."""
+
+    def __init__(self, size=0):
+        """This is how you create instantiation with size defined"""
+
+        self.__size = size
+        """This is how you create a private attribute in a class"""
+
+        if not (isinstance(size, int)):
+            raise TypeError("size must be an integer")
+
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        """Checks if size is an int & is greater than or equal to 0"""
+
+    def area(self):
+        """This is how you create a public instance method"""
+
+        sq = self.__size * self.__size
+        return(sq)
+        """Calculating size squared and returning it"""
