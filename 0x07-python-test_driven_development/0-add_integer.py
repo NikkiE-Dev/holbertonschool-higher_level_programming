69d9836cb96121709279417c51fbbd077e7624d8
NikkiE-Holberton <2495@holbertonschool.com>
2021-05-05 20:45:26 +0000

    Adding Function file & test file

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" This is a module to add 2 integers"""
+
+
+def add_integer(a, b=98):
+    """Function to do addition"""
+
+    if not isinstance(a, (int, float)):
+            raise TypeError("a must be an integer")
+
+    if not isinstance(b,(int, float)):
+            raise TypeError("b must be an integer")
+
+    if isinstance(a, float):
+        a = int(a)
+
+    if isinstance(b, float):
+        b = int(b)
+
+    return(a + b)
