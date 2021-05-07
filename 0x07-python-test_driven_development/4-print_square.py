6f23031a63fe4bb5cc57bbeeabd8dc9af5694afd
NikkiE-Holberton <2495@holbertonschool.com>
2021-05-07 04:36:01 +0000

    Changing order of code

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -12,14 +12,11 @@ def print_square(size):
     if isinstance(size, float) and size < 0:
         raise TypeError("size must be an integer")
 
+    if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
     if size < 0:
         raise ValueError("size must be >= 0")
     """Checks if size is an int & is greater than or equal to 0"""
-    if not isinstance(size, int):
-        raise TypeError("size must be an integer")
-
-    if size == 0:
-        print('')
-        return
-    for x in range(0, size):
+        for x in range(0, size):
         print(size * "#")
