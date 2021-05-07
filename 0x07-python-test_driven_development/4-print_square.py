aabdb228408719128b5321f06da84ba4242a31b1
NikkiE-Holberton <2495@holbertonschool.com>
2021-05-07 04:57:42 +0000

    Correcting docsting

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -10,13 +10,15 @@ def print_square(size):
     """This is how you create instantiation with size defined"""
 
     if isinstance(size, float) and size < 0:
+        """Checking type and if size is not neg num"""
         raise TypeError("size must be an integer")
 
     if not isinstance(size, int):
-            raise TypeError("size must be an integer")
+        """Checking type and if size is not neg num"""
+        raise TypeError("size must be an integer")
 
     if size < 0:
-        """Checks if size is an int & is greater than or equal to 0"""
+        """Checks if size is not neg num"""
         raise ValueError("size must be >= 0")
         for x in range(0, size):
         print(size * "#")
