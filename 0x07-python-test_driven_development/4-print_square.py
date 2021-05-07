9c8dc09fab8b210aa604c5c0d9155f89d6241aeb
NikkiE-Holberton <2495@holbertonschool.com>
2021-05-07 04:44:33 +0000

    Making PEP8 compliant

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -6,10 +6,9 @@ and that size is not a comionation of
 a float and is less than 0.
 """
 
-
 def print_square(size):
-    """This is how you create instantiation with size define\
-"""
+    """This is how you create instantiation with size defined"""
+
     if isinstance(size, float) and size < 0:
         raise TypeError("size must be an integer")
 
@@ -17,7 +16,7 @@ def print_square(size):
             raise TypeError("size must be an integer")
 
     if size < 0:
+        """Checks if size is an int & is greater than or equal to 0"""
         raise ValueError("size must be >= 0")
-    """Checks if size is an int & is greater than or equal to 0"""
         for x in range(0, size):
         print(size * "#")
