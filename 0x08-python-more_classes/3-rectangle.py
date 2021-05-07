1e9d541e483f261adeab48a2b609820f028ce028
NikkiE-Holberton <2495@holbertonschool.com>
2021-05-07 22:11:46 +0000

    Adding print rectangle file

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -59,7 +59,7 @@ class Rectangle:
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        if self.height == 0 or self.height == 0:
+        if self.width == 0 or self.height == 0:
             return ("")
         rect = ""
         for i in range(self.height):
