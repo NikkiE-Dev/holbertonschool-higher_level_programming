fd9192b9190a7957c42e047d386e0417e3aa4bd9
NikkiE-Holberton <2495@holbertonschool.com>
2021-05-09 23:28:52 +0000

    Making file PEP8 compliant

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-   try:
-      if len(a_dictionary) == 0:
-         return None
-      best =  max(a_dictionary, key=a_dictionary.get)
-      return best
+    try:
+        if len(a_dictionary) == 0:
+            return None
+        best = max(a_dictionary, key=a_dictionary.get)
+        return best
 
-   except:
-      return None
+    except:
+        return None
