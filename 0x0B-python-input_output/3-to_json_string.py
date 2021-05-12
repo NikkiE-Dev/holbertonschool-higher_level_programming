b196978d818a522357854b5a987899e48e89da24
NikkiE-Holberton <2495@holbertonschool.com>
2021-05-12 21:48:05 +0000

    Adding JSON string representation

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module returns the JSON
+representation of an object (string)
+"""
+
+import json
+
+def to_json_string(my_obj):
+    """This class defines the parameters of the function"""
+
+    return json.dumps(my_obj)
