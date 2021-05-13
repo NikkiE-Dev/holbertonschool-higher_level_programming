eef8fa25821e514141bdfa83492d8ba19a2d1d15
NikkiE-Holberton <2495@holbertonschool.com>
2021-05-13 01:18:14 +0000

    Adding file that retrieves a dictionary representation of a Student

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This module defines a class by students"""
+
+
+class Student:
+    """This class is defined by the public attr"""
+
+    def __init__(self, first_name, last_name, age):
+        """This is the instance attributes of the class"""
+
+        self.first_name = first_name
+        self.last_name = first_name
+        self.age = age
+
+    def to_json(self):
+        return {key: value for (key, value) in self.__dict__.items()
+                if key in list(self.__dict__.keys())}
