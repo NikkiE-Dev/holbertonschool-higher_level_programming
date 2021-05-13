b6f5f8afdadbf8bee85e8a304e251cf5c3432ad4
NikkiE-Holberton <2495@holbertonschool.com>
2021-05-13 00:29:44 +0000

    Making file PEP8 compliant

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,7 +2,9 @@
 """This module creates an Object from a JSON file"""
 
 
-import json, sys, os
+import os
+import sys
+import json
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
