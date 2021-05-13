e3236210466ca43ed7888c99651fda5e1951f77b
NikkiE-Holberton <2495@holbertonschool.com>
2021-05-13 20:45:56 +0000

    Adding additional tests

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -10,17 +10,17 @@ class TestMaxInteger(unittest.TestCase):
     def test(self):
         self.assertEqual(max_integer([1, 2, 3, 5]), 5)
 
-    def one_neg_number(self):
+    def test_one_neg_number(self):
         self.assertEqual(max_integer([-2, 0, 2, 4]), 4)
 
-    def all_neg_numbers(self):
-        self.assertEqual(max_integer([-4, -6, -8, -2]), 8)
+    def test_all_neg_numbers(self):
+        self.assertEqual(max_integer([-4, -6, -8, -2]), -2)
 
-    def max_in_midd(self):
+    def test_max_in_midd(self):
         self.assertEqual(max_integer([4, 6, 8, 2, 0]), 8)
 
-    def list_is_empty(self):
+    def test_list_is_empty(self):
         self.assertIsNone(max_integer([]))
 
-    def one_elem(self):
-        self.assertRaises(TypeError, max_integer(2))
+    def test_one_elem(self):
+        self.assertRaises(TypeError, max_integer([2]))
