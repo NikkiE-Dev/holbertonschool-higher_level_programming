7bf5d818c7a1dfac6ce8a0e6120331085d7c5c15
NikkiE-Holberton <2495@holbertonschool.com>
2021-05-13 20:19:18 +0000

    Correcting indention error

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -4,12 +4,14 @@
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
-class TestMaxInteger(unittest.TestCase):
 
-def test(self):
-    self.assertEqual(max_integer([1, 2, 3, 5]), 5)
-    self.assertEqual(max_integer([-2, 0, 2, 4]), 4)
-    self.assertRaises(TypeError, max_integer((1, 2)))
-    self.assertEqual(max_integer([8, 6, 4, 2]), 8)
-    self.assertRaises(TypeError, max_integer((1.14, 2.24)))
-    self.assertRaises(TypeError, max_integer(()))
+class TestMaxInteger(unittest.TestCase):
+    
+
+    def test(self):
+        self.assertEqual(max_integer([1, 2, 3, 5]), 5)
+        self.assertEqual(max_integer([-2, 0, 2, 4]), 4)
+        self.assertRaises(TypeError, max_integer((1, 2)))
+        self.assertEqual(max_integer([8, 6, 4, 2]), 8)
+        self.assertRaises(TypeError, max_integer((1.14, 2.24)))
+        self.assertRaises(TypeError, max_integer(()))
