0170d3441064e1663224803ae2859d5a6d06c463
NikkiE-Holberton <2495@holbertonschool.com>
2021-05-13 15:23:11 +0000

    Correcting self.last_name code

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -9,7 +9,7 @@ class Student:
         """This is the instance attributes of the class"""
 
         self.first_name = first_name
-        self.last_name = first_name
+        self.last_name = last_name
         self.age = age
 
     def to_json(self):
