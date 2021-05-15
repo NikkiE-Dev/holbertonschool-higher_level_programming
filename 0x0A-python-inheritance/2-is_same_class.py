b437780b44041e07dffbbf1d9957d61a6ced055e
NikkiE-Holberton <2495@holbertonschool.com>
2021-05-15 23:15:19 +0000

    Adding code to check if obj is exactly an instance of the specified class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module is to create a class that inherits from list"""
+
+
+def is_same_class(obj, a_class):
+    """"Checks to see if the obj is exactly an instance of the specific class"""
+
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
