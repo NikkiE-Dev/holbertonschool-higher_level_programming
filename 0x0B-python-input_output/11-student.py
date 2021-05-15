88b7a55aab346cb674361b58175d864dac151a12
NikkiE-Holberton <2495@holbertonschool.com>
2021-05-15 19:05:26 +0000

    Adding json def

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""This module defines a class by students"""
+
+
+class Student:
+    """This class is defined by the public attr"""
+
+    def __init__(self, first_name, last_name, age):
+        """This is the instance attributes of the class"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return vars(self)
+
+        else:
+            newAttrs = {}
+            if isinstance(attrs, list):
+                for a in attrs:
+                    if hasattr(self, a):
+                        newAttrs[a] = getattr(self, a)
+                return newAttrs
+
+    def reload_from_json(self, json):
+        self.first_name = json['first_name']
+        self.last_name = json['last_name']
+        self.age = json['age']
