2f58520160169e964dafe7479675e3afc8281404
NikkiE-Holberton <2495@holbertonschool.com>
2021-05-17 23:19:43 +0000

    Adding square getter and setter

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -12,3 +12,15 @@ class Square(Rectangle):
     def __str__(self):
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
                                                  self.height)
+    @property
+    def size(self):
+        return self.height
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        if value <= 0:
+            raise ValueError('width must be > 0')
+        self.width = value
+        self.height = value
