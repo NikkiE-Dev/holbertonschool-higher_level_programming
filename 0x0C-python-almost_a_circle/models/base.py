89e6cab9d7e5cd373e80127fdeb9d910c1f1adfb
NikkiE-Holberton <2495@holbertonschool.com>
2021-05-18 01:14:22 +0000

    Adding json representation of list of dictionaries

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """This module is for Unittest is for Base"""
+import json
 
 
 class Base:
@@ -15,3 +16,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns the JSON string representation of list_dictionaries"""
+        if list_dictionaries is None or ():
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
