3aee24a0dbe60bac0e6840b49005d8d23f10ce08
NikkiE-Holberton <2495@holbertonschool.com>
2021-05-27 03:09:22 +0000

    Adding database to line 3

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,4 @@
 -- Converts database to UTF8
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-ALTER COLUMN `name`  CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER COLUMN `name` IN first_table CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
