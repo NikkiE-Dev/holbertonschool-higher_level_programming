50df47ba31d16ce0defca972831bc696f71c5556
NikkiE-Holberton <2495@holbertonschool.com>
2021-05-27 23:58:21 +0000

    Adding additioanl comments

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
--- Creating user with password and granting all privileges
-sudo mysql –u root –p
-IF NOT EXISTS CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY user_0d_1_pwd;
+-- Creating user with password
+-- Granting all privileges to user
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
