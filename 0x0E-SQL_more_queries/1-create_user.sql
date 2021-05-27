b2d37cb5fc1e1ac27875b329280a0221c4d93db2
NikkiE-Holberton <2495@holbertonschool.com>
2021-05-27 23:34:43 +0000

    Code to add user and grant all priv

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- Creating user with password and granting all privileges
+sudo mysql –u root –p
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
