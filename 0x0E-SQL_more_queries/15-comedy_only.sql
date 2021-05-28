0b0d2d7c0f8fbf88b17f4cfe5cd9cf9c50bd108e
NikkiE-Holberton <2495@holbertonschool.com>
2021-05-28 05:14:23 +0000

    Updating code

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -4,4 +4,4 @@ SELECT tv_shows.title AS title
     FROM tv_show_genres INNER JOIN tv_shows
     ON tv_show_genres.show_id = tv_shows.id
     INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
-    AND tv_shows.name = 'Comedy' ORDER BY title;
+    AND tv_genres.name = 'Comedy' ORDER BY title;
