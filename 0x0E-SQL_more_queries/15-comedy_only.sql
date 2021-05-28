16dc90e15b2a8f48276ec496c3d83a7a3e853076
NikkiE-Holberton <2495@holbertonschool.com>
2021-05-28 05:12:34 +0000

    Updating code

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,6 +1,6 @@
 --  Lists all Comedy shows in the database hbtn_0d_tvshows.
 -- The tv_genres table contains only one record where name = Comedy
-SELECT tv_genres.title AS title
+SELECT tv_shows.title AS title
     FROM tv_show_genres INNER JOIN tv_shows
     ON tv_show_genres.show_id = tv_shows.id
     INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
