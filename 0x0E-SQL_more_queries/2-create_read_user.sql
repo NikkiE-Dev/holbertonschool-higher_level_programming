53ceeb14bb8c8bebadce6be96fc78723a4da27ae
NikkiE-Holberton <2495@holbertonschool.com>
2021-05-28 00:22:47 +0000

    Correcting Grant access code

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,4 +2,4 @@
 -- Granting all privileges to user
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2 TO 'user_0d_1'@'localhost';
+GRANT SELECT ON hbtn_0d_2 TO 'user_0d_2'@'localhost';
