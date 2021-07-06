22684ab79039fae4565dd4c44389ba26e6807b3c
NikkiE-Holberton <2495@holbertonschool.com>
2021-07-06 18:11:55 +0000

    Code to create rectangle in Javascript

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let a;
+    let b;
+    let ShowRect = '';
+    for (a = 0; a < this.height; a++) {
+      if (a > 0) {
+        ShowRect += '\n';
+      }
+      for (b = 0; b < this.width; b++) {
+        ShowRect += 'X';
+      }
+    }
+  }
+}
+
+module.exports = Rectangle;
