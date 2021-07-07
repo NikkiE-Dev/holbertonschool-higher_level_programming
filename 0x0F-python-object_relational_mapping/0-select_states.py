c4b893d2eb677c317abf62cf062e99ce87b410dc
NikkiE-Holberton <2495@holbertonschool.com>
2021-07-07 01:37:57 +0000

    Correcting imports

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,9 +3,10 @@
 Lists all states from the database
 """
 import MySQLdb
-import sys
+from sys import argv
 
 if __name__ == "__main__":
+
     db = MySQLdb.connect(host='localhost', argv[1], argv[2], argv[3], port=3306)
     position = db.cursor()
     position.execute("SELECT * FROM states")
