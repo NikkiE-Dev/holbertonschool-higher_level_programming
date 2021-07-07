65979f6088c6767e2f8a19aa93fa0de17426f785
NikkiE-Holberton <2495@holbertonschool.com>
2021-07-07 04:21:44 +0000

    Adding code for square w/charprint

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle.js');
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (typeof c === 'undefined') {
+      c = 'X';
+    }
+    this.print(c);
+  }
+}
+
+module.exports = Square;
