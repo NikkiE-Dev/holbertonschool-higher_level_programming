c0228977239be9b47fe63c8b2592630bf6363679
NikkiE-Holberton <2495@holbertonschool.com>
2021-07-07 04:26:54 +0000

    Updated code from rectangle to square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle.js');
-class Square extends Rectangle {
+const square = require('./5-square.js');
+class Square extends square {
   constructor (size) {
     super(size, size);
   }
