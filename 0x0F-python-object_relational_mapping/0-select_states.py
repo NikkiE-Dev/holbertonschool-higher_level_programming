ba0580b8f1c6e21bb8aef6e39613492493927991
NikkiE-Holberton <2495@holbertonschool.com>
2021-07-07 01:23:22 +0000

    Correcting code

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,7 +8,7 @@ import sys
 if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', argv[1], argv[2], argv[3])
     position = db.cursor()
-    position.execute("SELECT * FROM states ORDER BY states.id")
+    position.execute("SELECT * FROM states")
     row = position.fetchall()
     for item in row:
         print(item)
